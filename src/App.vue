2066a7677ca82bc76fa0d496b9a53a003e6dd169
MaxPredathor <max.gilli98@gmail.com>
2023-11-21 11:14:13 +0100

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data(){
+      return{
+        store,
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+@use './assets/styles/partials/variables' as *;  
+
+
+</style>
